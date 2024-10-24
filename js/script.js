082d500e2ba6c5dd15ff5244bcd7b6f232e9f9ec
mitanshm680 <mitanshm680@gmail.com>
2024-10-24 14:46:37 -0700

    more updates to nav bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Typing effect
     const typedTextElement = document.getElementById('typed-text');
     const description = "I'm a Data Scientist and Software Engineer skilled in machine learning, artificial intelligence, and building scalable software solutions. I enjoy analyzing data, optimizing algorithms, and driving innovation to deliver impactful results.";
     let index = 0;
@@ -12,4 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     typeText();
+    
+    // Initial navbar check on page load
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    }
+});
+
+// Scroll effect
+document.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
 });
